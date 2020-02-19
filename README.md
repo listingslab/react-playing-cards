782c935184826fa840b263b060fe4482a4d6c291
Chris <chris@codeworx.com.au>
2020-02-20 08:38:45 +1000

    init storybook

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# react-playing-cards
+# React Playing Cards
+
 Customisable SVG based Playing Card graphics
+
+```bash
+yarn && yarn start
+```
+
+should __just work__ &trade;
---
+++ stories/2-SvgGraphics.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default {
+    title: 'SVG Graphics',
+    component: SvgGraphics,
+};
+
+export const SvgGraphics = () => (
+    <React.Fragment>
+        SvgGraphics
+    </React.Fragment>
+);
