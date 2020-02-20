40027f8ac16191493d11ae1b40bbb5084a1de881
Chris <chris@codeworx.com.au>
2020-02-20 12:38:43 +1000

    Good place to branch and release

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,23 @@
 import { addParameters } from '@storybook/react';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 console.log(INITIAL_VIEWPORTS)
+
+const newViewports = {
+    landscape: {
+        name: '800x600',
+        styles: {
+            width: '800px',
+            height: '600px',
+        },
+    },
+};
+
 addParameters({
     viewport: {
-        viewports: INITIAL_VIEWPORTS,
-        defaultViewport: 'iphone8p',
+        viewports: {
+            ...INITIAL_VIEWPORTS,
+            ...newViewports,
+        },
+        defaultViewport: 'landscape',
     },
 });
