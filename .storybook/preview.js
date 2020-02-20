ac40b130104ae8f897a702b976d7ed37bfe227c7
listingslab <listingslab@gmail.com>
2020-02-21 06:52:34 +1000

    Material UI

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,22 +1,19 @@
-import { addParameters } from '@storybook/react';
-import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { addParameters } from "@storybook/react";
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 // console.log(INITIAL_VIEWPORTS)
-const newViewports = {
-    landscape: {
-        name: '800x600',
-        styles: {
-            width: '800px',
-            height: '600px',
-        },
-    },
-};
 
 addParameters({
-    viewport: {
-        viewports: {
-            ...INITIAL_VIEWPORTS,
-            ...newViewports,
-        },
-        defaultViewport: 'iphone8p',
+  viewport: {
+    viewports: {
+      ...INITIAL_VIEWPORTS,
+      dumbfone: {
+        name: "Dumbfone",
+        styles: {
+          width: "738px",
+          height: "414px"
+        }
+      }
     },
+    defaultViewport: "dumbfone"
+  }
 });
---
+++ stories/1-MaterialUI.stories.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { ContainedButtons } from "../components/index";
+
+export default {
+  title: "React implementation of Google's Material Design System",
+  component: Buttons
+};
+
+export const Buttons = () => (
+  <React.Fragment>
+    <ContainedButtons />
+  </React.Fragment>
+);
