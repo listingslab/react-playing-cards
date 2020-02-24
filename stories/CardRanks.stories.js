8d83e15b46208ecbc52ff47b5db3c2173b7e90c0
Chris <chris@codeworx.com.au>
2020-02-24 13:31:16 +1000

    Card Ranks

---
+++ stories/CardRanks.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import {
+//     Rank
+// } from "../components";
+
+export default {
+    title: 'Card Ranks',
+}
+
+export const Ranks = () => (
+    <div>list of ranks</div>
+)
