70d7e98e848ab826bf80639a96a56de04f2e6903
Chris <chris@codeworx.com.au>
2020-02-27 14:09:48 +1000

    mobile view

--- Cards/Readme.jsx
+++ Cards/Readme.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect } from 'react';
 import {
+    CssBaseline,
     Typography,
 } from '@material-ui/core';
 
 export default function Readme() {
     useEffect(() => {
-        console.log('this is fired after DOM has updated')
+        // console.log('this is fired after DOM has updated')
     })
 
     return (
         <React.Fragment>
+            <CssBaseline />
             <Typography variant={`h5`}>
                 Cards
             </Typography>
